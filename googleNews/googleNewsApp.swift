bbd1370354787863b82d5f7c9e23c2be31776cd6
Jun3631 <et244157@gmail.com>
2022-10-05 18:01:26 +0800

    Initial Commit

---
+++ googleNews/googleNewsApp.swift
@@ -0,0 +1,17 @@
+//
+//  googleNewsApp.swift
+//  googleNews
+//
+//  Created by Jun3631 on 2022/10/5.
+//
+
+import SwiftUI
+
+@main
+struct googleNewsApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
